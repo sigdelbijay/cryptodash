858d1af58fb138bcef49fe1c094581d7c4704a9c
sigdelbjay <sigdelbjay@gmail.com>
2021-03-16 11:48:53 +1100

    Add app layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { AppLayout } from './AppLayout'
 
 function App() {
   return (
-    <div className="App">
-      <div>Welcome to cryptodash</div>
-    </div>
+    <AppLayout>
+      Welcome to cryptodash
+    </AppLayout>
   );
 }
 
---
+++ src/AppLayout.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const AppLayout = styled.div`
+  padding: 40px;
+`
