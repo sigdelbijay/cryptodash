026ab01e2445e815b77c2d56cede7656c4eadf0e
sigdelbjay <sigdelbjay@gmail.com>
2021-03-16 12:26:05 +1100

    Restructure app, add appbar and grid

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { AppLayout } from './AppLayout'
+import AppBar from './AppBar'
 
 function App() {
   return (
     <AppLayout>
+      <AppBar />
       Welcome to cryptodash
     </AppLayout>
   );
