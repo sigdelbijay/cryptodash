3c6ef109202e94a2e34a17aee9d758b9c52d6cf5
sigdelbjay <sigdelbjay@gmail.com>
2021-03-18 16:42:29 +1100

    Add loading content

--- src/App/App.js
+++ src/App/App.js
@@ -5,16 +5,19 @@ import { AppProvider } from './AppProvider'
 import WelcomeMessage from '../Settings/WelcomeMessage'
 import ConfirmButton from '../Settings/ConfirmButton';
 import Page from '../Shared/Page'
+import Content from '../Shared/Page'
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar />
-        <Page name="settings">
-          <WelcomeMessage />
-          <ConfirmButton />
-        </Page>
+        <Content>
+          <Page name="settings">
+            <WelcomeMessage />
+            <ConfirmButton />
+          </Page>
+        </Content>
       </AppProvider>
     </AppLayout>
   );
