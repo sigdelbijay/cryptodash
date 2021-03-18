f54ce3b5cd2b5320455b9346981b03ded7929c8f
sigdelbjay <sigdelbjay@gmail.com>
2021-03-19 10:48:27 +1100

    Create Tile

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -5,12 +5,12 @@ import { fontSize1, greenBoxShadow, color3} from '../Shared/Styles'
 
 const ConfirmButtonStyled = styled.div`
   margin: 20px;
-  color: ${color3}
-  ${fontSize1}
+  color: ${color3};
+  ${fontSize1};
   padding: 5px;
   cursor: pointer;
   &:hover {
-    ${greenBoxShadow}
+    ${greenBoxShadow};
   }
 `
 export const CenterDiv = styled.div`
