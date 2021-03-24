b972fa2b6fb25467120f3785b2c1ff3b5154148d
sigdelbjay <sigdelbjay@gmail.com>
2021-03-24 14:46:57 +1100

    Add chart select

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,13 +1,23 @@
 import ReactHighCharts from 'react-highcharts'
 import HighChartsConfig from './HighchartsConfig'
 import { AppContext } from '../App/AppProvider'
-import {Tile} from '../Shared/Tile'
+import { Tile } from '../Shared/Tile'
+import ChartSelect from './ChartSelect'
 
 export default function() {
   return (
     <AppContext.Consumer>
-      {({historical}) =>
+      {({historical, changeChartSelect}) =>
         <Tile>
+          <ChartSelect
+            defaultValue="months"
+            onChange={e => changeChartSelect(e.target.value)}
+          >
+            <option value="days"> Days </option>
+            <option value="weeks"> Weeks </option>
+            <option value="months"> Months </option>
+
+          </ChartSelect>
           {historical
             ? <ReactHighCharts config={HighChartsConfig(historical)}/>
             : <div>Loading Historical Data</div>
